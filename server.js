dfa2389bae79606971fc6c29e1b94de1c906a9e0
Michael Clarke <mikkaclarke@hotmail.com>
2021-04-16 12:27:22 +0100

    new approach test

--- server.js
+++ server.js
@@ -7,29 +7,37 @@
 // init project
 const express        = require( 'express' );
 var app              = express();
-const fs             = require('fs');
-const lighthouse     = require('lighthouse');
-const chromeLauncher = require('chrome-launcher');
+const fs             = require( 'fs' );
+const lighthouse     = require( 'lighthouse' );
+const chromeLauncher = require( 'chrome-launcher' );
+const axios          = require( 'axios' );
 
 app.get( '/', async ( req, res ) => {
   req.connection.setTimeout( 60 * 10 * 1000 );
 
-	let target = req.headers.referer;
+  let target         = req.headers.referer;
+  let responseTarget = req.headers.response_target;
+  let demoName       = req.headers.demo_name;
 
-	if ( ! target || '' === target ) {
-		res.send( 'No domain passed on.' );
-	}
+  if ( ! target || '' === target || ! responseTarget || '' === responseTarget ) {
+    res.status( 403 ).send( 'No domain passed on.' );
+    return;
+  }
 
-	const chrome   = await chromeLauncher.launch( { 
+  res.send( 'Domain is being analysed.' );
+
+  const chrome = await chromeLauncher.launch( { 
     chromeFlags: ['--headless']
   } );
-	const options  = {
+
+  const options = {
     logLevel: 'info', 
     output: 'html', 
     onlyCategories: ['performance'], 
     port: chrome.port
   };
-  const config   = {
+
+  const config = {
     extends: 'lighthouse:default',
     settings: {
       onlyAudits: [
@@ -40,36 +48,42 @@ app.get( '/', async ( req, res ) => {
       ],
     },
   };
-	const runnerResult = await lighthouse( target, options, config );
-    let data         = {
-      fcp : {
-        score: runnerResult.lhr.audits['first-contentful-paint'].score,
-        display: runnerResult.lhr.audits['first-contentful-paint'].displayValue,
-      },
-      lcp: {
-        score:  runnerResult.lhr.audits['largest-contentful-paint'].score,
-        display: runnerResult.lhr.audits['largest-contentful-paint'].displayValue,
-      },
-      fid: {
-        score: runnerResult.lhr.audits['max-potential-fid'].score,
-        display: runnerResult.lhr.audits['max-potential-fid'].displayValue,
-      }, 
-      cls: {
-        score: runnerResult.lhr.audits['cumulative-layout-shift'].score,
-        display: runnerResult.lhr.audits['cumulative-layout-shift'].displayValue,
-      },
-      score: runnerResult.lhr.categories.performance.score
-    }
 
-	let jsonOutput = JSON.stringify( data );
-	res.send( jsonOutput );
+  const runnerResult = await lighthouse( target, options, config );
 
-  console.log( 'Completed', jsonOutput );
+  let data = {
+    fcp : {
+      score: runnerResult.lhr.audits['first-contentful-paint'].score,
+      display: runnerResult.lhr.audits['first-contentful-paint'].displayValue,
+    },
+    lcp: {
+      score:  runnerResult.lhr.audits['largest-contentful-paint'].score,
+      display: runnerResult.lhr.audits['largest-contentful-paint'].displayValue,
+    },
+    fid: {
+      score: runnerResult.lhr.audits['max-potential-fid'].score,
+      display: runnerResult.lhr.audits['max-potential-fid'].displayValue,
+    }, 
+    cls: {
+      score: runnerResult.lhr.audits['cumulative-layout-shift'].score,
+      display: runnerResult.lhr.audits['cumulative-layout-shift'].displayValue,
+    },
+    score: runnerResult.lhr.categories.performance.score,
+    demo_name: demoName,
+  }
+
+  axios.post( responseTarget, data ).then( ( res ) => {
+    console.log( res )
+  } ).catch( ( error ) => {
+    console.error( error )
+  } );
+
   await chrome.kill();
 } );
 
 // listen for requests :)
-var listener = app.listen( process.env.PORT, function() {
+let port = process.env.PORT;
+var listener = app.listen( 64291, function() {
   console.log( 'Your app is listening on port ' + listener.address().port );
 } );
 
