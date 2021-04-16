f15c643e0029ba311eb2d330dc5e9db578413f4f
Michael Clarke <mikkaclarke@hotmail.com>
2021-04-16 19:06:05 +0100

    port 80

--- server.js
+++ server.js
@@ -90,6 +90,6 @@ app.get( '/', async ( req, res ) => {
 // listen for requests :)
 let port = process.env.PORT;
 // let port = 64291;
-var listener = app.listen( port, function() {
+var listener = app.listen( 80, function() {
   console.log( 'Your app is listening on port ' + listener.address().port );
 } );
