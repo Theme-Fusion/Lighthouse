8b78dd4586ad9bd3f0ee8e7941ff46cd3c02135a
Michael Clarke <mikkaclarke@hotmail.com>
2021-04-20 19:17:58 +0100

    add try catch

--- server.js
+++ server.js
@@ -13,81 +13,88 @@ const puppeteer      = require('puppeteer')
 const axios          = require( 'axios' );
 
 app.get( '/', async ( req, res ) => {
-  let target         = req.headers.referer;
-  let responseTarget = req.query.response;
-  let demoName       = req.query.demo;
+  try {
+    let target         = req.headers.referer;
+    let responseTarget = req.query.response;
+    let demoName       = req.query.demo;
 
-  if ( ! target || '' === target || ! responseTarget || '' === responseTarget ) {
-    res.status( 403 ).send( 'No domain passed on.' );
-    return;
-  }
+    if ( ! target || '' === target || ! responseTarget || '' === responseTarget ) {
+      res.status( 403 ).send( 'No domain passed on.' );
+      return;
+    }
 
-  const chromeFlags = ['--headless', '--disable-dev-shm-usag', '--disable-gpu', '--disable-extensions', '--disable-popup-blocking', '--no-sandbox', '--single-process' ];
+    const chromeFlags = ['--headless', '--disable-dev-shm-usag', '--disable-gpu', '--disable-extensions', '--disable-popup-blocking', '--no-sandbox', '--single-process' ];
 
-  res.send( 'Domain is being analysed.' );
+    res.send( 'Domain is being analysed.' );
 
-  const chrome = await puppeteer.launch( { args: ['--no-sandbox'] } ).catch( error => { console.error( 'Chrome failed...', error ); } );
+    const chrome = await puppeteer.launch( { args: ['--no-sandbox'] } ).catch( error => { console.error( 'Chrome failed...', error ); } );
 
-  const options = {
-    logLevel: 'info', 
-    disableStorageReset: true,
-    maxWaitForLoad: 60000,
-    chromeFlags: chromeFlags,
-    onlyAudits: [
-      'first-contentful-paint',
-      'largest-contentful-paint',
-      'max-potential-fid',
-      'cumulative-layout-shift',
-    ],
-    port: chrome.port
-  };
-
-  const config = {
-    extends: 'lighthouse:default',
-    settings: {
+    const options = {
+      logLevel: 'info', 
+      disableStorageReset: true,
+      maxWaitForLoad: 60000,
+      chromeFlags: chromeFlags,
       onlyAudits: [
         'first-contentful-paint',
         'largest-contentful-paint',
         'max-potential-fid',
         'cumulative-layout-shift',
       ],
-    },
-  };
+      port: chrome.port
+    };
 
-  const runnerResult = await lighthouse( target, options, config ).catch( error => { console.error( 'Lighthouse failed...', error ); } );
+    const config = {
+      extends: 'lighthouse:default',
+      settings: {
+        onlyAudits: [
+          'first-contentful-paint',
+          'largest-contentful-paint',
+          'max-potential-fid',
+          'cumulative-layout-shift',
+        ],
+      },
+    };
 
-  let data = {
-    fcp : {
-      score: runnerResult.lhr.audits['first-contentful-paint'].score,
-      display: runnerResult.lhr.audits['first-contentful-paint'].displayValue,
-    },
-    lcp: {
-      score:  runnerResult.lhr.audits['largest-contentful-paint'].score,
-      display: runnerResult.lhr.audits['largest-contentful-paint'].displayValue,
-    },
-    fid: {
-      score: runnerResult.lhr.audits['max-potential-fid'].score,
-      display: runnerResult.lhr.audits['max-potential-fid'].displayValue,
-    }, 
-    cls: {
-      score: runnerResult.lhr.audits['cumulative-layout-shift'].score,
-      display: runnerResult.lhr.audits['cumulative-layout-shift'].displayValue,
-    },
-    score: runnerResult.lhr.categories.performance.score,
-    demo_name: demoName,
-  }
+    const runnerResult = await lighthouse( target, options, config ).catch( error => { console.error( 'Lighthouse failed...', error ); } );
 
-  await chrome.close();
+    let data = {
+      fcp : {
+        score: runnerResult.lhr.audits['first-contentful-paint'].score,
+        display: runnerResult.lhr.audits['first-contentful-paint'].displayValue,
+      },
+      lcp: {
+        score:  runnerResult.lhr.audits['largest-contentful-paint'].score,
+        display: runnerResult.lhr.audits['largest-contentful-paint'].displayValue,
+      },
+      fid: {
+        score: runnerResult.lhr.audits['max-potential-fid'].score,
+        display: runnerResult.lhr.audits['max-potential-fid'].displayValue,
+      }, 
+      cls: {
+        score: runnerResult.lhr.audits['cumulative-layout-shift'].score,
+        display: runnerResult.lhr.audits['cumulative-layout-shift'].displayValue,
+      },
+      score: runnerResult.lhr.categories.performance.score,
+      demo_name: demoName,
+    }
 
-  await chrome.kill();
+    await chrome.close();
 
-  axios.post( responseTarget, data ).then( ( res ) => {
+    await chrome.kill();
+
+    axios.post( responseTarget, data ).then( ( res ) => {
+      return;
+    } ).catch( ( error ) => {
+      console.error( error )
+    } );
+
     return;
-  } ).catch( ( error ) => {
-    console.error( error )
-  } );
-
-  return;
+  } catch (e) {
+    await chrome.close();
+    console.log(e);
+  } finally {
+    await chrome.close();
+  }
 } );
 
 // listen for requests :)
